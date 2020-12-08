b2d7a123924668fff3ac30ee42c0113ec689c50c
Madhu <enometh@net.meer>
2020-12-08 11:38:47 +0530

    doc/conf.py: point sphinx to local python docs

--- doc/conf.py
+++ doc/conf.py
@@ -4,4 +4,4 @@ copyright = u'2010,2014,2016,2019,2020, Russell Stuart'
 version = '1.0.8'
 release = '1.0.8'
 extensions = ['sphinx.ext.intersphinx']
-intersphinx_mapping = {'python': ('http://docs.python.org/2.7', None)}
+intersphinx_mapping = {'python': ('/scratch/share/docs/Python/', None)}
